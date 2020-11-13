1bd3b69648e155ca519232d6666242227cf7cb16
Kestutis Kurienius <kurkestutis@gmail.com>
2020-11-13 12:29:16 +0200

    Main schema

---
+++ src/components/Game.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Board from './Board';
+
+const Game = () => {
+
+    const handleClick = () => {
+
+    }
+
+    const jumpTo = () => {
+
+    }
+
+    const renderMoves = () => {
+        
+    }
+
+    return (
+        <Board onclick={handleClick}/>
+    )
+}
+
+export default Game;
